d287f877fe8ac2aafe1f0098177af44c548b993f
Tim Snedden <spolsiondude50@gmail.com>
2019-04-09 15:57:27 -0700

    Finalizing Visuals/Selling Of Items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,69 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class Inventory : MonoBehaviour {
-    public List<ButtonsComp> buttonList = new List<ButtonsComp>();
+    public List<ButtonsComp> ShopItems = new List<ButtonsComp>();
+    [HideInInspector]
+    public List<string> playerInv;
+    public List<Texture> backgroundImages;
+    public RawImage rawImage;
     public Text ShopName;
     public Text ShopDescr;
+    public Text ScreenInv;
+    public Text ScreenMoney;
     private int PlayerMoney;
-    public Text Money;
-    public Text PlayerInv;
-    public List<string> playerInv;
     // Use this for initialization
     void Start() {
-        switch (Random.Range(1, 4))
+        switch (Random.Range(0, 4))
         {
             case 0:
-                ShopName.text = "0";
+                ShopName.text = "Gnome Depot";
+                rawImage.texture = backgroundImages[0];
                 break;
             case 1:
-                ShopName.text = "1";
+                ShopName.text = "The Shadeland";
+                rawImage.texture = backgroundImages[1];
                 break;
             case 2:
-                ShopName.text = "2";
+                ShopName.text = "VoidShow";
+                rawImage.texture = backgroundImages[2];
                 break;
             case 3:
-                ShopName.text = "3";
+                ShopName.text = "Corecords";
+                rawImage.texture = backgroundImages[3];
                 break;
         }
-        ShopDescr.text = "Welcome would you like to buy one of these Items";
-        ShopDescr.resizeTextForBestFit = true;
+        ShopDescr.text = "Would you like to buy one of these Items" + "\n"+ "To Buy Click The Buttons" +"\n"+
+                          "To Sell press 1 through 5 to sell the weapon in the order of the shop";
         ShopDescr.color = Color.white;
         PlayerMoney = Random.Range(0, 546);
-        PlayerInv.color = Color.white;
-        PlayerInv.fontSize = 39;
-        Money.color = Color.white;
-        Money.resizeTextForBestFit = true;
-        for (int i = 0; i < buttonList.Count; i++)
+        ScreenInv.color = Color.white;
+        ScreenInv.fontSize = 25;
+        ScreenMoney.color = Color.white;
+        for (int i = 0; i < ShopItems.Count; i++)
         {
-         buttonList[i].Obj.GetComponentInChildren<Text>().text = buttonList[i].objText + buttonList[i].objVal;
+         ShopItems[i].Obj.GetComponentInChildren<Text>().text = "Weapon: " + ShopItems[i].objText + "\n"+ 
+                "Cost: " + ShopItems[i].objVal;
         }
-        buttonList[0].Obj.onClick.AddListener(() => AddtoInv(0));
-        buttonList[1].Obj.onClick.AddListener(() => AddtoInv(1));
-        buttonList[2].Obj.onClick.AddListener(() => AddtoInv(2));
-        buttonList[3].Obj.onClick.AddListener(() => AddtoInv(3));
-        buttonList[4].Obj.onClick.AddListener(() => AddtoInv(4));
+        ShopItems[0].Obj.onClick.AddListener(() => AddtoInv(0));
+        ShopItems[1].Obj.onClick.AddListener(() => AddtoInv(1));
+        ShopItems[2].Obj.onClick.AddListener(() => AddtoInv(2));
+        ShopItems[3].Obj.onClick.AddListener(() => AddtoInv(3));
+        ShopItems[4].Obj.onClick.AddListener(() => AddtoInv(4));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Money.text = "Your Money Is: " + PlayerMoney;
-    }
-    public void AddtoInv(int i)
-    {
-        if (PlayerMoney > buttonList[i].objVal)
+        ScreenMoney.text = "Your Money Is: " + PlayerMoney;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            playerInv.Add(buttonList[i].objText);
-            PlayerInv.text = "";
-            PlayerMoney = PlayerMoney - buttonList[i].objVal;
+            Remove(ShopItems[0].objText);
+            PlayerMoney = PlayerMoney + 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Remove(ShopItems[1].objText);
+            PlayerMoney = PlayerMoney + 15;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Remove(ShopItems[2].objText);
+            PlayerMoney = PlayerMoney + 60;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Remove(ShopItems[3].objText);
+            PlayerMoney = PlayerMoney + 85;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Remove(ShopItems[4].objText);
+            PlayerMoney = PlayerMoney + 30;
+        }
+    }
+    public void AddtoInv(int val)
+    {
+        if (PlayerMoney > ShopItems[val].objVal)
+        {
+            playerInv.Add(ShopItems[val].objText);
+            ScreenInv.text = "";
+            PlayerMoney = PlayerMoney - ShopItems[val].objVal;
         }
         else
         {
-            return;
+            Debug.Log("You Dont Have Enough Money");
         }
         foreach(string msg in playerInv)
         {
-            PlayerInv.text += msg.ToString() + "\n";
+            ScreenInv.text += msg.ToString() + "\n";
+
+        }
+    }
+    public void Remove(string val)
+    {
+        playerInv.Remove(val);
+        ScreenInv.text = "";
+        foreach (string msg in playerInv)
+        {
+            ScreenInv.text += msg.ToString() + "\n";
+
         }
     }
 }
