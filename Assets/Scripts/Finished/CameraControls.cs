75fbaf2351fdb5254a1d17e6c7057f9c2c5e498f
Tim Snedden <spolsiondude50@gmail.com>
2019-04-23 16:17:42 -0700

    FinishedBasicSaving

---
+++ Assets/Scripts/Finished/CameraControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public List<Cameras> cam = new List<Cameras>();
+    void Start()
+    {
+    }
+    void Update()
+    {
+
+        switch (Input.inputString)
+        {
+            case "1":
+                CameraSwitching(0);
+                break;
+            case "2":
+                CameraSwitching(1);
+                break;
+        }
+    }
+    public void CameraSwitching(int val)
+    {
+        if (val == 0)
+        {
+            cam[val].camera.gameObject.SetActive(true);
+            cam[1].camera.gameObject.SetActive(false);
+        }
+        if (val == 1)
+        {
+            cam[val].camera.gameObject.SetActive(true);
+            cam[0].camera.gameObject.SetActive(false);
+        }
+    }
+}
+[System.Serializable]
+public class Cameras
+{
+    public Camera camera;
+}
--- Assets/Scripts/Testing/PlayerControls.cs
+++ Assets/Scripts/Testing/PlayerControls.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerControls : MonoBehaviour
 {
     private Rigidbody player;
     private int speed = 40;
+    public Camera playerCam;
     void Start()
     {
         player = GetComponent<Rigidbody>();
@@ -13,30 +13,29 @@ public class PlayerControls : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (playerCam == Camera.current)
         {
-            player.AddForce(transform.forward * speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            player.AddForce(transform.forward * -speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            player.AddForce(transform.right * -speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            player.AddForce(transform.right * speed);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            speed = speed * 2;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                player.AddForce(transform.forward * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.AddForce(transform.forward * -speed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                player.AddForce(transform.right * -speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                player.AddForce(transform.right * speed);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                speed = speed * 2;
+            }
         }
     }
-}
-[System.Serializable]
-public class Cameras
-{
-    public Camera Camera;
 }
