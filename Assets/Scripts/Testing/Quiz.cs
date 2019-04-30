6b57acb7fac78f78ed74a7bd8d33e5f02dcba608
Tim Snedden <spolsiondude50@gmail.com>
2019-04-30 15:58:09 -0700

    WorkingOnSection3

--- Assets/Scripts/Testing/Quiz.cs
+++ Assets/Scripts/Testing/Quiz.cs
@@ -6,10 +6,11 @@ public class Quiz : MonoBehaviour
 {
     public List<GameObject> objects = new List<GameObject>();
     public List<Text> texts = new List<Text>();
-    public int correctVals;
+    public int correctVal;
+    public int randomVal;
     void Start()
     {
-        for(int i = 0; i < objects.Count; i++)
+        for (int i = 0; i < objects.Count; i++)
         {
             objects[i].gameObject.GetComponent<Renderer>().material.color = Color.white;
         }
@@ -17,29 +18,30 @@ public class Quiz : MonoBehaviour
         {
             if (objects[t].GetComponent<QuizObj>().objChild.CorrectVal == 1)
             {
-                correctVals = correctVals + 1;
+                correctVal = correctVal + 1;
             }
             else if (objects[t].GetComponent<QuizObj>().objChild.CorrectVal == 0)
             {
                 Debug.Log("Not One of the Values");
             }
         }
+        randomVal = Random.Range(0, 12 - correctVal);
         switch (Random.Range(0, 2))
         {
             case 0:
-                texts[0].text = "Left Choice" + correctVals;
-                texts[1].text = "Middle Choice" + Random.Range(0,12 - correctVals);
-                texts[2].text = "Right Choice" + Random.Range(0, 12 - correctVals);
+                texts[0].text = "Left Choice " + correctVal;
+                texts[1].text = "Middle Choice " + randomVal;
+                texts[2].text = "Right Choice " + randomVal;
                 break;
             case 1:
-                texts[0].text = "Left Choice" + Random.Range(0,12 - correctVals);
-                texts[1].text = "Middle Choice" + correctVals;
-                texts[2].text = "Right Choice" + Random.Range(0, 12 - correctVals);
+                texts[0].text = "Left Choice " + randomVal;
+                texts[1].text = "Middle Choice " + correctVal;
+                texts[2].text = "Right Choice " + randomVal;
                 break;
             case 2:
-                texts[0].text = "Left Choice" + Random.Range(0,12 - correctVals);
-                texts[1].text = "Middle Choice" + Random.Range(0, 12 - correctVals);
-                texts[2].text = "Right Choice" + correctVals;
+                texts[0].text = "Left Choice " + randomVal;
+                texts[1].text = "Middle Choice " + randomVal;
+                texts[2].text = "Right Choice " + correctVal;
                 break;
         }
     }
